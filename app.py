f7b7637e51a021a5141e16a93cd74cb6cbb64928
nisarg <nisarg1463@gmail.com>
2022-01-29 12:43:23 +0530

    second commit

--- app.py
+++ app.py
@@ -4,20 +4,20 @@ import requests
 
 app = Flask(__name__)
 
-apiKey = "kMl9TQ4wS6S90ElOEWy_rxeaboaf9huxq_sf89uduZg"
+apiKey = 'kMl9TQ4wS6S90ElOEWy_rxeaboaf9huxq_sf89uduZg'
 
 
 @app.route('/locations' , methods=['GET'])
 def locations():
-    coordinates = request.args["query"].split()
+    coordinates = request.args['query'].split()
     print(len(coordinates))
     # 'https://discover.search.hereapi.com/v1/discover?at=52.5228,13.4124&q=petrol+station&limit=5&apiKey=kMl9TQ4wS6S90ElOEWy_rxeaboaf9huxq_sf89uduZg'
 
     data = requests.get(
-        f"https://browse.search.hereapi.com/v1/browse?at={coordinates[0]},{coordinates[1]}&apiKey={apiKey}"
+        f'https://browse.search.hereapi.com/v1/browse?at={coordinates[0]},{coordinates[1]}&apiKey={apiKey}&in=circle:{coordinates[0]},{coordinates[1]};r={coordinates[2]}'
     ).json()
     return data
 
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
